55bdb6432fba89b5999f99562cdda8f72e26ee24
José Enrique <otipip90@gmail.com>
2022-02-02 19:46:01 -0500

    created initial creation task

--- src/addTask.js
+++ src/addTask.js
@@ -1,9 +1,29 @@
 const { v4 } = requiere('uuid');
+const AWS = requiere('aws-sdk');
 
 const addTask = async (event) => {
-    const { title, description } = event.body;
+    const dynamodb = new Date();
+    const { title, description } = JSON.parse(event.body);
     const createAt = new Date();
     const id = v4();
+
+    const newTask = {
+        title,
+        description,
+        createAt,
+    };
+
+    await dynamodb
+        .put({
+            TableName: 'TaskTable',
+            Item: newTask,
+        })
+        .promise();
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(newTask),
+    };
 };
 
 module.exports = {
